27ee52c147a143299a1871cfd66c17aa63a45242
Lvl3Mage <lvlthreemage@gmail.com>
2024-08-16 05:10:20 +0000

    Deploying to gh-pages from @ Lvl3Mage/CameraManagement2D@b97213557d983040420151b594156811c8e20b70 🚀

--- search/variables_2.js
+++ search/variables_2.js
@@ -1,11 +1,10 @@
 var searchData=
 [
-  ['cameracontrollers_0',['cameraControllers',['../a00062.html#a386bacf8cbf914e77c2b15a38a6bcfc8',1,'Lvl3Mage::CameraManagement2D::CameraModuleManager']]],
-  ['camerapadding_1',['cameraPadding',['../a00094.html#a45f26ba5eb8610b45663ec7d96f0c046',1,'Lvl3Mage::CameraManagement2D::TrackerCameraModule']]],
-  ['clamp_2',['clamp',['../a00066.html#a584cba17bac117d27dd6d6309db6adea',1,'Lvl3Mage.CameraManagement2D.CameraPanModule.clamp()'],['../a00094.html#a639c4e2194a524638e6140c0cdf665fc',1,'Lvl3Mage.CameraManagement2D.TrackerCameraModule.clamp()']]],
-  ['clampmode_3',['clampMode',['../a00082.html#a5bb125936455d27e60fd2cdf14a615a7',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
-  ['clampxaxis_4',['clampXAxis',['../a00082.html#a3b4dcea8b7ffd4cd62149aeab4eef326',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
-  ['clampyaxis_5',['clampYAxis',['../a00082.html#a92508979a7c56f2f823958c8a801a1cd',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
-  ['clampzoom_6',['clampZoom',['../a00082.html#a752579e0620d728f2899b058471bf0f7',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
-  ['controllercamera_7',['controllerCamera',['../a00058.html#a1b632cad2d9bef1aea1d0ceb4b5b0d18',1,'Lvl3Mage::CameraManagement2D::CameraController']]]
+  ['camerapadding_0',['cameraPadding',['../a00094.html#a45f26ba5eb8610b45663ec7d96f0c046',1,'Lvl3Mage::CameraManagement2D::TrackerCameraModule']]],
+  ['clamp_1',['clamp',['../a00066.html#a584cba17bac117d27dd6d6309db6adea',1,'Lvl3Mage.CameraManagement2D.CameraPanModule.clamp()'],['../a00094.html#a639c4e2194a524638e6140c0cdf665fc',1,'Lvl3Mage.CameraManagement2D.TrackerCameraModule.clamp()']]],
+  ['clampmode_2',['clampMode',['../a00082.html#a5bb125936455d27e60fd2cdf14a615a7',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
+  ['clampxaxis_3',['clampXAxis',['../a00082.html#a3b4dcea8b7ffd4cd62149aeab4eef326',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
+  ['clampyaxis_4',['clampYAxis',['../a00082.html#a92508979a7c56f2f823958c8a801a1cd',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
+  ['clampzoom_5',['clampZoom',['../a00082.html#a752579e0620d728f2899b058471bf0f7',1,'Lvl3Mage::CameraManagement2D::CameraStateClamp']]],
+  ['controllercamera_6',['controllerCamera',['../a00058.html#a1b632cad2d9bef1aea1d0ceb4b5b0d18',1,'Lvl3Mage::CameraManagement2D::CameraController']]]
 ];
--- search/variables_3.js
+++ search/variables_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['drawgizmos_0',['drawGizmos',['../a00066.html#a15c29e04df84f75e69bd287cc2602d79',1,'Lvl3Mage.CameraManagement2D.CameraPanModule.drawGizmos()'],['../a00094.html#a3b9c6fd017c287968721167d25c0ca52',1,'Lvl3Mage.CameraManagement2D.TrackerCameraModule.drawGizmos()']]]
+  ['displayname_0',['displayName',['../a00062.html#a61172f416814e7d01a9d18557270c986',1,'Lvl3Mage::CameraManagement2D::CameraModuleManager']]],
+  ['drawgizmos_1',['drawGizmos',['../a00066.html#a15c29e04df84f75e69bd287cc2602d79',1,'Lvl3Mage.CameraManagement2D.CameraPanModule.drawGizmos()'],['../a00094.html#a3b9c6fd017c287968721167d25c0ca52',1,'Lvl3Mage.CameraManagement2D.TrackerCameraModule.drawGizmos()']]]
 ];
