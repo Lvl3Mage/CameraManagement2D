db800d0070a3d975ab8bcd4d5c6b3e1a42650edb
Karl <lvlthreemage@gmail.com>
2024-08-19 02:58:57 +0200

    Version bump

--- Runtime/TrackerCameraModule.cs
+++ Runtime/TrackerCameraModule.cs
@@ -50,7 +50,7 @@ namespace Lvl3Mage.CameraManagement2D
 		/// <summary>
 		/// The component used to clamp the camera's state to certain bounds.
 		/// </summary>
-		[SerializeField] CameraStateClamp clamp = new();
+		[SerializeField] CameraStateClamp clamp;
 		/// <summary>
 		/// Whether to draw gizmos in the editor for visualizing clamping bounds.
 		/// </summary>
